a4a3da1634eecb0637b4d231773980ad9aab2c4f
Kevin <kevin@kevinhubert.com>
2015-12-15 21:54:55 -0600

    changing colors of icons in off canvas footer

--- wp-content/themes/dashoftexas/offcanvas.php
+++ wp-content/themes/dashoftexas/offcanvas.php
@@ -10,8 +10,8 @@
 	<li><a href="/contact">Contact</a></li>
 </ul>
 <footer class="footer">
-	<svg class="icon icon-facebook2"><use xlink:href="<?php echo ( site_url() ); ?>/wp-content/themes/dashoftexas/assets/img/svg/symbol-defs.svg#icon-facebook2"></use></svg>
-	<svg class="icon icon-twitter2"><use xlink:href="<?php echo ( site_url() ); ?>/wp-content/themes/dashoftexas/assets/img/svg/symbol-defs.svg#icon-twitter2"></use></svg>
-	<svg class="icon icon-pinterest2"><use xlink:href="<?php echo ( site_url() ); ?>/wp-content/themes/dashoftexas/assets/img/svg/symbol-defs.svg#icon-pinterest2"></use></svg>
-	<svg class="icon icon-instagram"><use xlink:href="<?php echo ( site_url() ); ?>/wp-content/themes/dashoftexas/assets/img/svg/symbol-defs.svg#icon-instagram"></use></svg>
+	<a href="https://facebook.com/dashoftexas" alt="Dash of Texas Facebook" target="_blank"><svg class="icon icon-facebook2"><use xlink:href="<?php echo ( site_url() ); ?>/wp-content/themes/dashoftexas/assets/img/svg/symbol-defs.svg#icon-facebook2"></use></svg>
+	<a href="https://twitter.com/dashoftexas" alt="Dash of Texas Twitter" target="_blank"><svg class="icon icon-twitter2"><use xlink:href="<?php echo ( site_url() ); ?>/wp-content/themes/dashoftexas/assets/img/svg/symbol-defs.svg#icon-twitter2"></use></svg>
+	<a href="https://pinterest.com/dashoftexas" alt="Dash of Texas Pinterst" target="_blank"><svg class="icon icon-pinterest2"><use xlink:href="<?php echo ( site_url() ); ?>/wp-content/themes/dashoftexas/assets/img/svg/symbol-defs.svg#icon-pinterest2"></use></svg>
+	<a href="https://instagram.com/dashoftexas" alt="Dash of Texas Instagram" target="_blank"><svg class="icon icon-instagram"><use xlink:href="<?php echo ( site_url() ); ?>/wp-content/themes/dashoftexas/assets/img/svg/symbol-defs.svg#icon-instagram"></use></svg>
 </footer>
