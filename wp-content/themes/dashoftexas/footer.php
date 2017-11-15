1592366d36d072eab89aba33e2310a264df00944
Kevin Hubert <iamkevinhubert@gmail.com>
2017-11-14 19:05:59 -0600

    adding google script

--- wp-content/themes/dashoftexas/footer.php
+++ wp-content/themes/dashoftexas/footer.php
@@ -3,6 +3,14 @@
 <!-- SCRIPTS -->
 <script src="<?php site_url(); ?>/wp-content/themes/dashoftexas/js/owl.carousel.min.js"></script>
 
+<!-- Google Ads -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-2693491020856121",
+    enable_page_level_ads: true
+  });
+</script>
 
 <!-- REPLACE IN SCRIPT FILE BEFORE GO LIVE -->
 <script type="text/javascript">
